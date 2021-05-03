9623bf1bef139295345c88efaa1ad11613e261f1
Itsukiworks <67408564+Itsukiworks@users.noreply.github.com>
2021-05-03 21:41:58 +0700

    Update Burn.js

--- App/Command/Fun/Burn.js
+++ App/Command/Fun/Burn.js
@@ -1,5 +1,4 @@
 const { Client, Message } = require('@components/DiscordClient') // eslint-disable-line
-const { RichEmbed } = require('discord.js')
 
 /**
  * @param {Client} client
@@ -10,7 +9,11 @@ module.exports = (client, message, args) => {
   const guild = message.guild
   // Ambil member dari ID dan Mention, apabila gaada yanmg ketemu terakhir ambil
   // id author/pengirim pesan
-  message.channel.send(${member.user.tag} ', Kamu saya bakar pakai api neraka ya! :fire::fire::fire:')
+  const member = message.mentions.members.fisrt() ||
+        guild.members.get(args[0]) ||
+        guild.members.get(message.author.id)
+  
+  message.channel.send('${member.user.tag}, Kamu saya bakar pakai api neraka ya! :fire::fire::fire:')
     .then(msg => {
       message.channel.send('https://tenor.com/view/coki-pardede-majelis-lucu-on-fire-dancing-gif-16618017')
     })
