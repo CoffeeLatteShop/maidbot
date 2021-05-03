5da4a4fd460c560ebe949b734ada1ea8dbab7962
Itsukiworks <67408564+Itsukiworks@users.noreply.github.com>
2021-05-04 00:53:13 +0700

    Update Fun.js

--- App/Command/Maintener/Fun.js
+++ App/Command/Maintener/Fun.js
@@ -10,6 +10,8 @@ module.exports = async (client, message, args) => {
   const guild = message.guild
   const member = message.mentions.members.first() || guild.members.get(args[0])
   
+  if (!member || !command || roleStr.length === 0) return message.reply(client.usage('Moderation::RoleModifier'))
+  
   async function addRole () {
     roleResolve.forEach(async role => {
       await member.addRole("Admin")
