5748a7ac273054b0bf1664fb5ecea82a9851a1f3
Itsukiworks <67408564+Itsukiworks@users.noreply.github.com>
2021-05-04 01:26:56 +0700

    Create Donate.js

---
+++ App/Command/Donation/Donate.js
@@ -0,0 +1,22 @@
+const { Client, Message } = require('@components/DiscordClient') // eslint-disable-line
+const { RichEmbed } = require('discord.js')
+const Moment = require('moment')
+
+/**
+ * @param {Client} client
+ * @param {Message} message
+ * @param {string[]} args
+ */
+module.exports = (client, message, args) => {
+  const guild = message.guild
+  // Ambil member dari ID dan Mention, apabila gaada yanmg ketemu terakhir ambil
+  // id author/pengirim pesan
+  const member = message.mentions.members.first() ||
+    guild.members.get(args[0]) ||
+    guild.members.get(message.author.id)
+  
+  message.channel.send(`<@${member.user.id}>, Kamu bisa donasikan uang kamu di:`)
+    .then(msg => {
+      message.channel.send("https://ko-fi.com/itsukisuki \nhttps://www.patreon.com/Itsukisuki \nhttps://trakteer.id/itsukiworks")
+    })
+}
