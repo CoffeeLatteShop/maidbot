a455f46317b83a54df7f9cdc02aea7f1cbf3804e
Itsukiworks <67408564+Itsukiworks@users.noreply.github.com>
2021-05-04 13:40:16 +0700

    Create Ctash.js

---
+++ App/Command/Fun/Ctash.js
@@ -0,0 +1,24 @@
+const { Client, Message } = require('@components/DiscordClient') // eslint-disable-line
+const { RichEmbed } = require('discord.js')
+const Moment = require('moment')
+
+/**
+ * @param {Client} client
+ * @param {Message} message
+ * @param {string[]} args
+ */
+module.exports = (client, message, args) => {
+  const guild = message.guild
+  // Ambil member dari ID dan Mention, apabila gaada yanmg ketemu terakhir ambil
+  // id author/pengirim pesan
+  const member = message.mentions.members.first() ||
+    guild.members.get(args[0]) ||
+    guild.members.get(message.author.id)
+  let ctash = ['https://tenor.com/view/the-rock-whooping-whip-whipping-belt-gif-13188986', 'https://tenor.com/view/baroncorbin-baron-corbin-strap-whip-gif-8213092', 'https://tenor.com/view/family-guy-whip-angry-dad-gif-15082707', 'https://tenor.com/view/whip-some-ass-serious-face-seriously-gif-14815466']
+  let ctashR = ctash[Math.floor(Math.random() * ctash.length)]
+  
+  message.channel.send(`<@${member.user.id}> ***KENA CTASH !!!***`)
+    .then(msg => {
+      message.channel.send(ctashR)
+    })
+}
