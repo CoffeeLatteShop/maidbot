3adefd665d7cf36ab12847282112e29ffb824249
Itsukiworks <67408564+Itsukiworks@users.noreply.github.com>
2021-05-04 13:47:13 +0700

    Update Ctash.js

--- App/Command/Fun/Ctash.js
+++ App/Command/Fun/Ctash.js
@@ -14,7 +14,7 @@ module.exports = (client, message, args) => {
   const member = message.mentions.members.first() ||
     guild.members.get(args[0]) ||
     guild.members.get(message.author.id)
-  let ctash = ['https://tenor.com/view/the-rock-whooping-whip-whipping-belt-gif-13188986', 'https://tenor.com/view/baroncorbin-baron-corbin-strap-whip-gif-8213092', 'https://tenor.com/view/family-guy-whip-angry-dad-gif-15082707', 'https://tenor.com/view/whip-some-ass-serious-face-seriously-gif-14815466']
+  let ctash = ['https://tenor.com/view/the-rock-whooping-whip-whipping-belt-gif-13188986', 'https://tenor.com/view/baroncorbin-baron-corbin-strap-whip-gif-8213092', 'https://tenor.com/view/family-guy-whip-angry-dad-gif-15082707', 'https://tenor.com/view/whip-some-ass-serious-face-seriously-gif-14815466', 'https://tenor.com/view/whip-fire-whip-fire-cowboy-burn-gif-4478689', 'https://tenor.com/view/fire-whip-ivan-can-norman-gif-19279459', 'https://tenor.com/view/whip-punish-ouch-no-angry-gif-15788990']
   let ctashR = ctash[Math.floor(Math.random() * ctash.length)]
   
   message.channel.send(`<@${member.user.id}> ***KENA CTASH !!!***`)
