78130a16c7f44f5148b4948537b4c0352239b5d2
Itsukiworks <67408564+Itsukiworks@users.noreply.github.com>
2021-05-04 14:27:11 +0700

    Update Punch.js

--- App/Command/Fun/Punch.js
+++ App/Command/Fun/Punch.js
@@ -14,9 +14,13 @@ module.exports = (client, message, args) => {
   const member = message.mentions.members.first() ||
     guild.members.get(args[0]) ||
     guild.members.get(message.author.id)
+  let punch = ['https://tenor.com/view/anime-punch-knockout-wasted-smack-gif-11451829', 'https://tenor.com/view/anime-smash-lesbian-punch-wall-gif-4790446', 'https://tenor.com/view/anime-punch-mad-angry-gif-15580060', 'https://tenor.com/view/tgggg-anime-punch-gif-13142581', 'https://tenor.com/view/anime-punch-punch-in-the-face-gif-14210784', 'https://tenor.com/view/hajimenoippo-ippo-punch-boxing-anime-gif-5247335', 'https://tenor.com/view/double-punch-anime-double-punch-toaru-majutsu-no-index-a-certain-magical-index-touma-gif-20988936', 'https://tenor.com/view/anime-kid-punch-toy-gif-10161883', 'https://tenor.com/view/anime-punch-gif-7679409', 'https://tenor.com/view/anime-punch-mad-angry-gif-16962926', 'https://tenor.com/view/nichijou-uppercut-punch-die-anime-gif-8932977'];
+  let gebug = punch[Math.floor(Math.random() * punch.length)];
+  let quote = ['Adaw', 'Oof', 'Plak', 'Aduduh', 'MAMAK', 'Aku ngadu ah', 'Bonyok ga tuh', 'WKWK'];
+  let quoter = quote[Math.floor(Math.random() * quote.length)]
   
-  message.channel.send(`<@${member.user.id}> minta di tumbuk . . .`)
+  message.channel.send(`<@${member.user.id}> kena tonjok! ${quoter}!`)
     .then(msg => {
-      message.channel.send(`https://tenor.com/view/anime-punch-knockout-wasted-smack-gif-11451829`)
+      message.channel.send(gebug)
     })
 }
