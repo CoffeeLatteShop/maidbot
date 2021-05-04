d3d9ca36d0e2ff245236312d05a17735993e092e
Itsukiworks <67408564+Itsukiworks@users.noreply.github.com>
2021-05-04 14:05:45 +0700

    Update Hug.js

--- App/Command/Fun/Hug.js
+++ App/Command/Fun/Hug.js
@@ -15,7 +15,7 @@ module.exports = (client, message, args) => {
     guild.members.get(args[0]) ||
     guild.members.get(message.author.id)
   let hug = ['https://tenor.com/view/hug-anime-gif-11074788', 'https://tenor.com/view/anime-hug-sweet-love-gif-14246498', 'https://tenor.com/view/hug-cuddle-comfort-love-friends-gif-5166500', 'https://tenor.com/view/hug-anime-gif-4898650', 'https://tenor.com/view/hug-anime-love-sweet-tight-hug-gif-7324587', 'https://tenor.com/view/chiya-urara-meirochou-anime-saku-gif-8995974', 'https://tenor.com/view/hug-anime-sweet-gif-10195705', 'https://tenor.com/view/abra%C3%A7o-hug-miss-you-gif-14903944', 'https://tenor.com/view/anime-hug-catgirl-neko-gif-9383138', 'https://tenor.com/view/toilet-bound-hanakokun-anime-anime-hug-gif-16831471']
-  let huggy = hug[Math.floor(Math.random() * slaps.length)]
+  let huggy = hug[Math.floor(Math.random() * hug.length)]
       
   message.channel.send(`<@${member.user.id}> di hug sama <@${message.author.id}> ><`)
     .then(msg => {
